caefc4ef1702287552555a4668cefa9d957045f9
twant <twant@kippaustin.org>
2017-01-09 13:09:16 -0500

    editted for class practice

--- models/model.rb
+++ models/model.rb
@@ -1,10 +1,3 @@
-#basic calculator
-def add(number1, number2)
-  number1+number2
-end
-
-#define a new method called multiply. define @product inside of your application controller
-
 
 #life calculator
 def years_alive(birthday)
@@ -70,30 +63,4 @@ end
 
 
 
-def klossify(phrase)
-  phrase.gsub("c","k").gsub("C","K")
-end
 
-
- def piglatinize(word)
-    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-    vowels = ["a", "e", "i", "o", "u"]
- 
-      if non_pig_latin_words.include?(word)
-        word
-       elsif vowels.include? word[0]
-        word << "ay"
-       else
-         consonants = ""
-       while !vowels.include?(word[0])
-         consonants << word[0]
-         word = word.split("")[1..-1].join
-       end
-       word + consonants + 'ay'
-   end
- end
- 
-
- def to_pig_latin(string)
-   string.split.collect{|word| piglatinize(word)}.join(" ")
- end
